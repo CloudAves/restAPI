96bc31fd3abd4c3b44aee475e7dd660f2058d622
killercodemonkey <bengtler@gmail.com>
2014-06-17 23:17:41 +0200

    add login, refresh and logout method

--- endpoints/authentication.js
+++ endpoints/authentication.js
@@ -3,19 +3,60 @@ define([
     'crypto',
     'models/authentication',
     'models/user',
-    'appConfig'
-], function (jwt, crypto, Authentication, User, appConfig) {
+    'appConfig',
+    'node-promise'
+], function (jwt, crypto, Authentication, User, appConfig, promise) {
+    var Promise = promise.Promise;
+
+    function generateAuthentication(user) {
+        var $q = new Promise(),
+            secret = crypto.randomBytes(128).toString('base64'),
+            userData = {
+                id: user.userId,
+                username: user.username,
+                email: user.email,
+                created: user.created,
+                permissions: user.permissions,
+                secret: secret,
+                expiresInMinutes: appConfig.tokenExpiresInMinutes,
+                tokenType: 'Bearer'
+            },
+            accessToken,
+            refreshToken,
+            auth;
+
+        accessToken = jwt.sign(userData, appConfig.secret, { expiresInMinutes: appConfig.tokenExpiresInMinutes });
+        userData.accessToken = accessToken;
+
+        refreshToken = jwt.sign(userData, appConfig.secret);
+        userData.refreshToken = refreshToken;
+
+        auth = new Authentication({
+            userId: userData.id,
+            secret: secret,
+            accessToken: accessToken,
+            refreshToken: refreshToken
+        });
+
+        auth.save(function (err) {
+            if (err) {
+                $q.reject({
+                    error: 'failed_authentication'
+                });
+            } else {
+                $q.resolve(userData);
+            }
+        });
+
+        return $q;
+    }
+
     return {
         post : {
             // /authentication
             'login': {
                 permissions: [],
                 exec: function (req, res) {
-                    var userData,
-                        accessToken,
-                        refreshToken,
-                        secret = crypto.randomBytes(128).toString('base64');
-
                     if (req.body.login && req.body.password) {
                         User.findOne({
                             $or: [{
@@ -29,38 +70,11 @@ define([
                                     error: 'user_not_found'
                                 });
                             } else if (user.checkPassword(req.body.password)) {
-                                userData = {
-                                    id: user.userId,
-                                    username: user.username,
-                                    email: user.email,
-                                    created: user.created,
-                                    permissions: user.permissions,
-                                    secret: secret,
-                                    expiresInMinutes: appConfig.tokenExpiresInMinutes,
-                                    tokenType: 'Bearer'
-                                };
 
-                                accessToken = jwt.sign(userData, appConfig.secret, { expiresInMinutes: appConfig.tokenExpiresInMinutes });
-                                userData.accessToken = accessToken;
-
-                                refreshToken = jwt.sign(userData, appConfig.secret);
-                                userData.refreshToken = refreshToken;
-
-                                var authentication = new Authentication({
-                                    userId: userData.id,
-                                    secret: secret,
-                                    accessToken: accessToken,
-                                    refreshToken: refreshToken
-                                });
-
-                                authentication.save(function (err) {
-                                    if (err) {
-                                        res.json({
-                                            error: 'failed_authentication'
-                                        });
-                                    } else {
-                                        res.json(userData);
-                                    }
+                                generateAuthentication(user).then(function (userData) {
+                                    res.json(userData);
+                                }, function (err) {
+                                    res.json(400, err);
                                 });
                             } else {
                                 res.json({
@@ -75,13 +89,60 @@ define([
                     }
                 }
             },
+            'refresh': {
+                permissions: ['user'],
+                exec: function (req, res) {
+                    Authentication.findOne({
+                        accessToken: req.user.accessToken,
+                        userId: req.user.userId,
+                        secret: req.user.secret
+                    }, function (err, authentication) {
+                        if (err) {
+                            res.send(err);
+                        } else {
+                            if (authentication.refreshToken === req.body.refreshToken) {
+                                authentication.remove(function (err) {
+                                    if (err) {
+                                        res.send(400, err);
+                                    } else {
+                                        generateAuthentication(req.user).then(function (userData) {
+                                            res.json(userData);
+                                        }, function (err) {
+                                            res.json(400, err);
+                                        });
+                                    }
+                                });
+                            } else {
+                                res.send(400, {
+                                    error: 'wrong_refresh_token'
+                                });
+                            }
+                            authentication.remove(function () {
+                                res.end();
+                            });
+                        }
+                    });
+                }
+            }
         },
-        'logout': {
-            permissions: ['user'],
-            exec: function (req, res) {
-                var accessToken = req.get('Authorization');
-
-
+        get: {
+            'logout': {
+                permissions: ['user'],
+                exec: function (req, res) {
+                    Authentication.findOne({
+                        accessToken: req.user.accessToken,
+                        userId: req.user.userId,
+                        secret: req.user.secret
+                    }, function (err, authentication) {
+                        if (err) {
+                            res.send(err);
+                        } else {
+                            authentication.remove(function () {
+                                res.end();
+                            });
+                        }
+                    });
+                }
             }
         }
     };
